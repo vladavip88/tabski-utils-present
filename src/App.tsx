1e54fdb752fda63888f0cd01b19b6913344706d8
Vladimir Prsendic <admin@admins-MacBook-Pro.local>
2025-06-04 16:46:31 +0200

    small update

--- src/App.tsx
+++ src/App.tsx
@@ -7,126 +7,286 @@ import {
   calculateOrderCheckItemBasePriceUI,
   calculateOrderCheckItemTaxesUI,
   calculateOrderCheckItemTotalPriceUI,
+  calculateOrderCheckItemRefundedPriceUI,
+  calculateOrderCheckItemVoidedPriceUI,
+  calculateOrderCheckSubtotalPriceUI,
+  calculateOrderCheckTaxesUI,
+  calculateOrderCheckDiscountsHashMapUI,
 } from "@tabski-organization/tabski-utils";
 
 const highlight = (code: string) =>
   Prism.highlight(code, Prism.languages.json, "json");
 
 const ORDER = {
-  id: "681dba0df2d434e0acf6ce58",
-  additionalInfo: {
-    scheduleAt: null,
-    specialInstructions: "",
-  },
-  application: "CONSUMER",
-  consumerContactInfo: null,
   consumerId: "clb0z1cmg0575bhmruuws7tun",
   consumerName: "vladimir",
-  deliveryInfo: null,
+  createdAt: "2025-06-04T07:43:23.193Z",
   employeeId: null,
   employeeName: null,
   fee: 99,
+  id: "683ff913abc7c9f777a28513",
   isOpenTab: false,
+  merchantId: "cl78yyrut89323lmrexxfoo9p",
+  multimerchantId: null,
+  referenceId: "683FF913UZVS",
+  type: "DINE_IN",
+  updatedAt: "2025-06-04T07:43:23.193Z",
+  application: "CONSUMER",
   items: [
     {
       acceptedKdsId: null,
       amount: 3,
       cashDiscount: 0,
-      chitName: "Carbonara",
+      chitName: "Lentils bolognese",
+      createdAt: "2025-06-04T07:43:23.192Z",
       delayedUntil: null,
-      dynamicPrice: {
-        id: "clwz0z1ov0001iqgilqwjrwc5",
-        name: "Happy hour",
-        type: "DECREASED_PERCENTAGE",
-        amount: 10,
-      },
-      externalItemId: "6H9TKS1ETPZ90",
-      itemKdsIds: [],
+      externalItemId: null,
+      id: "683ff913abc7c9f777a28514",
+      itemKdsIds: ["clpr2tmxa13765anmphr1ebhlu"],
       itemPrinterIds: [],
-      menuCategoryId: "cloo8g88i4477s9mrwpwouxtp",
-      menuCategoryName: "Pastas",
-      menuId: "clhg54dwo31829ydmr28hr1zoq",
-      menuItemId: "cloo7c7mh0332qc161r9ipycm",
+      menuCategoryId: "cm1g7f7kc006v8gq7546k3lyy",
+      menuCategoryName: "House Specials",
+      menuId: "clpqw9gwc6734anmparkklvhr",
+      menuItemId: "cm1g7n7ix00718gq7tyrlt9di",
       modifiers: [
         {
-          groupExternalId: null,
+          amount: 1,
+          cashDiscount: 0,
+          chitName: "Chicken",
           externalId: null,
-          groupName: "Condiments",
-          name: "Sriracha",
-          chitName: "Sriracha",
-          price: 50,
+          groupExternalId: null,
+          groupName: "Special extras",
+          name: "Chicken",
+          price: 99,
+        },
+        {
+          amount: 1,
+          cashDiscount: 0,
+          chitName: "Beef",
+          externalId: null,
+          groupExternalId: null,
+          groupName: "Special extras",
+          name: "Beef",
+          price: 99,
+        },
+        {
+          amount: 1,
+          cashDiscount: 0,
+          chitName: "Cheese",
+          externalId: null,
+          groupExternalId: null,
+          groupName: "Special extras",
+          name: "Cheese",
+          price: 99,
+        },
+        {
+          amount: 1,
+          cashDiscount: 0,
+          chitName: "Mustard",
+          externalId: null,
+          groupExternalId: null,
+          groupName: "Pizza extras",
+          name: "Mustard",
+          price: 0,
         },
       ],
-      name: "Carbonara",
+      name: "Lentils bolognese",
+      note: "",
       orderedByConsumerId: null,
       orderedByConsumerName: null,
-      price: 400,
-      priceTieredName: null,
-      revenueCenter: "cl78yyrva89553lmri4aqqzzt",
-      status: "IN_PROGRESS",
-      tableKdsIds: [],
-      tableName: "Urbanspace Table 1",
-      tableOrderType: "Bar Pickup",
+      price: 500,
+      priceTieredName: "",
+      revenueCenter: "Food",
+      status: "PENDING",
+      tableKdsIds: ["clquyakay000wbn39zp5r356t"],
+      tableName: "novi sto",
+      tableOrderType: "Dine-in",
       tablePrinterIds: [],
-      tableSection: "Main Section",
+      tableSection: "Main Dining",
       taxes: [
         {
           amount: 10,
-          externalId: "P8WM9RN2J552M",
-          id: "cloo78qxz0144of1669v3clyr",
+          externalId: null,
+          id: "clpr2s8o313566anmpbeu6vg9q",
           isInclusive: false,
           isRemoved: false,
-          name: "tax 1",
+          name: "Food and Drink",
           type: "PERCENTAGE",
         },
       ],
+      updatedAt: "2025-06-04T07:43:23.192Z",
       voidReason: null,
-      id: "681dba0df2d434e0acf6ce59",
-      createdAt: "2025-05-09T08:17:25.525+0000",
-      updatedAt: "2025-05-09T08:17:25.525+0000",
+      dynamicPrice: null,
+    },
+    {
+      acceptedKdsId: null,
+      amount: 2,
+      cashDiscount: 0,
+      chitName: "Coconut chickpeas",
+      createdAt: "2025-06-04T07:43:23.193Z",
+      delayedUntil: null,
+      externalItemId: null,
+      id: "683ff913abc7c9f777a28515",
+      itemKdsIds: ["clpr2tmxa13765anmphr1ebhlu"],
+      itemPrinterIds: [],
+      menuCategoryId: "cm1g7f7kc006v8gq7546k3lyy",
+      menuCategoryName: "House Specials",
+      menuId: "clpqw9gwc6734anmparkklvhr",
+      menuItemId: "cm1g7j229006z8gq7bt6o56ov",
+      modifiers: [],
+      name: "Coconut chickpeas",
+      note: "",
+      orderedByConsumerId: null,
+      orderedByConsumerName: null,
+      price: 999,
+      priceTieredName: "",
+      revenueCenter: "Food",
+      status: "PENDING",
+      tableKdsIds: ["clquyakay000wbn39zp5r356t"],
+      tableName: "novi sto",
+      tableOrderType: "Dine-in",
+      tablePrinterIds: [],
+      tableSection: "Main Dining",
+      taxes: [
+        {
+          amount: 10,
+          externalId: null,
+          id: "clpr2s8o313566anmpbeu6vg9q",
+          isInclusive: false,
+          isRemoved: false,
+          name: "Food and Drink",
+          type: "PERCENTAGE",
+        },
+      ],
+      updatedAt: "2025-06-04T07:43:23.193Z",
+      voidReason: null,
+      dynamicPrice: null,
+    },
+    {
+      acceptedKdsId: null,
+      amount: 4,
+      cashDiscount: 0,
+      chitName: "Falafel bowl",
+      createdAt: "2025-06-04T07:43:23.193Z",
+      delayedUntil: null,
+      externalItemId: null,
+      id: "683ff913abc7c9f777a28516",
+      itemKdsIds: ["clpr2tmxa13765anmphr1ebhlu"],
+      itemPrinterIds: [],
+      menuCategoryId: "cm1g7f7kc006v8gq7546k3lyy",
+      menuCategoryName: "House Specials",
+      menuId: "clpqw9gwc6734anmparkklvhr",
+      menuItemId: "cm1g7gsdz006x8gq7sk69yo37",
+      modifiers: [],
+      name: "Falafel bowl",
+      note: "",
+      orderedByConsumerId: null,
+      orderedByConsumerName: null,
+      price: 1100,
+      priceTieredName: "",
+      revenueCenter: "Food",
+      status: "PENDING",
+      tableKdsIds: ["clquyakay000wbn39zp5r356t"],
+      tableName: "novi sto",
+      tableOrderType: "Dine-in",
+      tablePrinterIds: [],
+      tableSection: "Main Dining",
+      taxes: [
+        {
+          amount: 10,
+          externalId: null,
+          id: "cm7mep2ko00z5zizgefakt4tc",
+          isInclusive: true,
+          isRemoved: false,
+          name: "inclusive percentage",
+          type: "PERCENTAGE",
+        },
+      ],
+      updatedAt: "2025-06-04T07:43:23.193Z",
+      voidReason: null,
+      dynamicPrice: null,
     },
   ],
-  merchantId: "cl78yyrut89323lmrexxfoo9p",
-  multimerchantId: null,
-  referenceId: "681DBA0DREMG",
-  type: "DINE_IN",
-  createdAt: "2025-05-09T08:17:25.526+0000",
-  updatedAt: "2025-05-09T08:17:25.526+0000",
 };
 const ORDER_CHECK = {
-  id: "681dba0df2d434e0acf6ce5a",
+  id: "683ff913abc7c9f777a28517",
   checks: [
     {
       consumerId: "clb0z1cmg0575bhmruuws7tun",
-      discounts: [],
-      isPaid: false,
+      discounts: [
+        {
+          amount: 200,
+          id: "cly5wa7ds01zygebomibmdk6y",
+          name: "test 123123",
+          type: "FIXED_AMOUNT_OFF",
+        },
+        {
+          amount: 10,
+          id: "clx8ytk9f0001re77cs8gfi9x",
+          name: "vlada test",
+          type: "FIXED_PERCENTAGE_OFF",
+        },
+      ],
       items: [
         {
-          amount: 2,
+          amount: 3,
           discounts: [],
-          orderItemId: "681dba0df2d434e0acf6ce59",
+          orderItemId: "683ff913abc7c9f777a28514",
           refundAmount: 0,
           refundFailedAmount: 0,
           voidAmount: 0,
-          id: "681dba15f2d434e0acf6ce60",
-          createdAt: "2025-05-09T08:17:25.658+0000",
-          updatedAt: "2025-05-09T08:17:25.658+0000",
+          voidPaidAmount: 0,
+          id: "683ff91babc7c9f777a2851f",
+          createdAt: "2025-06-04T07:43:23.349+0000",
+          updatedAt: "2025-06-04T07:43:23.349+0000",
+        },
+        {
+          amount: 2,
+          discounts: [],
+          orderItemId: "683ff913abc7c9f777a28515",
+          refundAmount: 0,
+          refundFailedAmount: 0,
+          voidAmount: 0,
+          voidPaidAmount: 0,
+          id: "683ff91babc7c9f777a28520",
+          createdAt: "2025-06-04T07:43:23.350+0000",
+          updatedAt: "2025-06-04T07:43:23.350+0000",
+        },
+        {
+          amount: 4,
+          discounts: [],
+          orderItemId: "683ff913abc7c9f777a28516",
+          refundAmount: 0,
+          refundFailedAmount: 0,
+          voidAmount: 0,
+          voidPaidAmount: 0,
+          id: "683ff91babc7c9f777a28521",
+          createdAt: "2025-06-04T07:43:23.350+0000",
+          updatedAt: "2025-06-04T07:43:23.350+0000",
         },
       ],
       paymentProvider: "CLOVER",
+      paymentStatus: "NONE",
       paymentTransactions: [],
-      referenceId: "681DBA0DESZP",
+      referenceId: "683FF913VEGW",
       tip: 100,
-      id: "681dba15f2d434e0acf6ce5f",
-      createdAt: "2025-05-09T08:17:25.658+0000",
-      updatedAt: "2025-05-09T08:17:25.658+0000",
+      id: "683ff91babc7c9f777a2851e",
+      createdAt: "2025-06-04T07:43:23.350+0000",
+      updatedAt: "2025-06-04T07:43:23.350+0000",
     },
   ],
   merchantId: "cl78yyrut89323lmrexxfoo9p",
-  orderId: "681dba0df2d434e0acf6ce58",
-  createdAt: "2025-05-09T08:17:25.659+0000",
-  updatedAt: "2025-05-09T08:17:25.659+0000",
+  orderId: "683ff913abc7c9f777a28513",
+  createdAt: "2025-06-04T07:43:23.350+0000",
+  updatedAt: "2025-06-04T07:43:23.350+0000",
 };
+
+const ListItem = ({ label, value }) => (
+  <li className="flex justify-between gap-x-6 py-1 hover:bg-blue-100">
+    <p className="text-sm/6 font-semibold text-gray-900">{label}</p>
+    <p className="text-gray-500">{value}</p>
+  </li>
+);
 
 function App() {
   const [order, setOrder] = useState(JSON.stringify(ORDER, null, 2));
@@ -206,39 +366,112 @@ function App() {
           !orderError &&
           !orderCheckError && (
             <div>
-              <p className="text-lg font-semibold">
-                CHECK ITEM BASE PRICE:{" "}
-                {parsedOrderChecks.checks[0].items.map((item) =>
-                  calculateOrderCheckItemBasePriceUI({
-                    orderCheckItem: item,
-                    orderItem: parsedOrder.items.find(
-                      (orderItem) => orderItem.id === item.orderItemId
-                    )! as any,
+              <ul
+                role="list"
+                className="divide-y divide-gray-300 bg-gray-50 p-2 mb-4"
+              >
+                <ListItem
+                  label="Order check subtotal"
+                  value={calculateOrderCheckSubtotalPriceUI({
+                    orderCheckItems: parsedOrderChecks.checks[0].items,
+                    orderItems: parsedOrder.items,
+                  })}
+                />
+                <ListItem
+                  label="Order check texes"
+                  value={calculateOrderCheckTaxesUI({
+                    orderCheck: parsedOrderChecks.checks[0],
+                    orderItems: parsedOrder.items,
+                  })}
+                />
+                {/* <ListItem
+                  label="Order check discounts"
+                  value={calculateOrderCheckDiscountsUI({
+                    orderCheck: parsedOrderChecks.checks[0],
+                    orderItems: parsedOrder.items,
+                  })}
+                /> */}
+                <ListItem
+                  label="Order check discounts hash map"
+                  value={calculateOrderCheckDiscountsHashMapUI({
+                    orderCheck: parsedOrderChecks.checks[0],
+                    orderItems: parsedOrder.items,
                   })
-                )}
-              </p>
-              <p className="text-lg font-semibold">
-                CHECK ITEM TAXES PRICE:{" "}
-                {parsedOrderChecks.checks[0].items.map((item) =>
-                  calculateOrderCheckItemTaxesUI({
-                    orderCheckItem: item,
-                    orderItem: parsedOrder.items.find(
-                      (orderItem) => orderItem.id === item.orderItemId
-                    )! as any,
-                  })
-                )}
-              </p>
-              <p className="text-lg font-semibold">
-                CHECK ITEM TOTAL PRICE:{" "}
-                {parsedOrderChecks.checks[0].items.map((item) =>
-                  calculateOrderCheckItemTotalPriceUI({
-                    orderCheckItem: item,
-                    orderItem: parsedOrder.items.find(
-                      (orderItem) => orderItem.id === item.orderItemId
-                    )! as any,
-                  })
-                )}
-              </p>
+                    .map((discount) => `${discount.name}: ${discount.price}`)
+                    .join(", ")}
+                />
+                <ListItem
+                  label="Order check tip"
+                  value={parsedOrderChecks.checks[0].tip}
+                />
+              </ul>
+              {parsedOrderChecks.checks[0].items.map((item) => {
+                const orderItem = parsedOrder.items.find(
+                  (i) => i.id === item.orderItemId
+                );
+
+                return (
+                  <div key={item.id} className="mb-4">
+                    <ul
+                      role="list"
+                      className="divide-y divide-gray-300 bg-gray-50 p-2"
+                    >
+                      <ListItem
+                        label="Order Item ID"
+                        value={item.orderItemId}
+                      />
+                      <ListItem label="Order Check Item ID" value={item.id} />
+                      <ListItem
+                        label="Base Price (amount included, modifiers not included)"
+                        value={calculateOrderCheckItemBasePriceUI({
+                          orderCheckItem: item,
+                          orderItem,
+                        })}
+                      />
+                      <ListItem
+                        label="Modifiers Price"
+                        value={orderItem.modifiers.reduce(
+                          (acc, curr) =>
+                            acc +
+                            (curr.price * curr.amount || 1) * orderItem.amount,
+                          0
+                        )}
+                      />
+
+                      <ListItem
+                        label="Taxes"
+                        value={calculateOrderCheckItemTaxesUI({
+                          orderCheckItem: item,
+                          orderItem,
+                        })}
+                      />
+
+                      <ListItem
+                        label="Total"
+                        value={calculateOrderCheckItemTotalPriceUI({
+                          orderCheckItem: item,
+                          orderItem,
+                        })}
+                      />
+
+                      <ListItem
+                        label="Refunded Price"
+                        value={calculateOrderCheckItemRefundedPriceUI({
+                          orderCheckItem: item,
+                          orderItem,
+                        })}
+                      />
+                      <ListItem
+                        label="Voided Price"
+                        value={calculateOrderCheckItemVoidedPriceUI({
+                          orderCheckItem: item,
+                          orderItem,
+                        })}
+                      />
+                    </ul>
+                  </div>
+                );
+              })}
             </div>
           )}
       </div>
